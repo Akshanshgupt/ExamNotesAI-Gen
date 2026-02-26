4b2a511a4c59345067900758208f8ee0ea6af8aa
Akshanshgupt <akkineni662@gmail.com>
2026-02-26 15:20:05 +0530

    Update CORS origin for production environment

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.post(
 );
 
 app.use(cors(
-    {origin:"http://localhost:5173",
+    {origin:"https://examnotesai-gen-client-33z8.onrender.com",
         credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
